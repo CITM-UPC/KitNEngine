3f86b6e04f8ddadf652213764f0cdca71a83853f
Roger Puchol Celma <49079760+Ropuce@users.noreply.github.com>
2024-11-11 13:47:26 +0100

    Corregida eliminació accidental dels Assets del projecte

---
+++ bin/Assets/Shaders/MyVertexShader.glsl
@@ -0,0 +1,17 @@
+#version 330 core
+layout (location = 0) in vec3 aPos; // the position of the vertex
+layout (location = 1) in vec2 aUV;
+
+out vec2 texCoords;
+
+uniform mat4 projection = mat4(1.0);
+uniform mat4 view = mat4(1.0);
+uniform mat4 model = mat4(1.0);
+
+void main()
+{
+    gl_Position = projection * view * model * vec4(aPos.xyz, 1.0);
+    //gl_Position = model * vec4(aPos.xyz, 1.0);
+    
+    texCoords = vec2(aUV.x, -aUV.y);
+}
